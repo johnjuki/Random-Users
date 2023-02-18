18623d3108d5e2979719c5fe199a1ee3cba66045
John Njuki <njukijohn2000@gmail.com>
2023-02-18 09:58:29 +0300

    update the styling

--- app.py
+++ app.py
@@ -7,20 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    url = "https://randomuser.me/api/?results=100&gender=male"
+    url = "https://randomuser.me/api/?results=100&inc=name,email,dob,phone,picture&gender=male"
     response = requests.get(url)
     if response.status_code == 200:
-        json_response = response.text
-        data = json.loads(json_response)
+        json_string = response.text
+        data = json.loads(json_string)
 
-        # Extract first names from the results list
-        names = []
-        for result in data['results']:
-            full_name = result['name']['first'] + ' ' + result['name']['last']
-            names.append(full_name)
-
-        # Pass the first names to the template for display
-        return render_template('home.html', names=names)
+        # Pass the results to the template for display
+        return render_template('home.html', results=data['results'])
     else:
         return "Error retrieving random users."
 
